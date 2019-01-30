84f67164380fcf1248a993d0ff6dd37d34c0b509
Scott Vojik <scott.vojik@gmail.com>
2019-01-30 12:27:54 -0800

    initial commit/PR

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// we'll need axios
+// we'll need axios 
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
@@ -6,3 +6,4 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+  
